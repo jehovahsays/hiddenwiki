0d9dc2fe476e0d6f75bf567be2246d39ee8b7393
jehovahsays <morgansbyers@gmail.com>
2017-02-21 18:57:35 -0500

    hiddenwiki

--- index.php
+++ index.php
@@ -10,9 +10,10 @@ header_remove( 'X-Powered-By' );
 ?>
 <html>
 <head>
+<title>Chat</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 </head>
 <body>
-<iframe src="http://www.jehovahsays.net/blackhole/browserquest/web/" style="border: 0; position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%"></iframe>
+<iframe src="http://www.jehovahsays.net:3000" style="border: 0; position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%"></iframe>
 </body>
 </html>
