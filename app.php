7e9a1f168bed6703cd01725f1f87ee8aed2123b8
jehovahsays <morgansbyers@gmail.com>
2017-03-10 23:18:14 -0500

    search

---
+++ app.php
@@ -0,0 +1,19 @@
+<?php
+include(realpath(getenv('DOCUMENT_ROOT')) .'/blackhole/blackhole.php');
+echo "<!DOCTYPE html>\n"; 
+echo "<html lang=\"en\"><head>	\n"; 
+echo "<meta charset=\"UTF-8\">\n"; 
+echo "<title>Search - @jehovahsays</title>\n"; 
+echo "<meta name=\"viewport\" content=\"width=device-width\">\n"; 
+echo "<meta name=\"theme-color\" content=\"red\">\n"; 
+echo "<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\">\n"; 
+echo "\n"; 
+echo "</head>\n"; 
+echo "<body>\n"; 
+echo "<center>\n"; 
+echo "<iframe width=\"100%\" height=\"415\" src=\"https://www.youtube.com/embed/YtciqGrM8xw\" frameborder=\"0\" allowfullscreen></iframe>\n";
+echo "</center>\n";
+echo "<a rel=\"nofollow\" href=\"https://www.jehovahsays.net/blackhole/\"></a>\n";
+echo "</body>	\n"; 
+echo "</html>\n";
+?>
